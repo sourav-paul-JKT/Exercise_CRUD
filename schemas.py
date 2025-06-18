93c03a69493edfec03add918927c81ed7a20316b
sourav-paul-JKT <sourav.paul@jktech.com>
2025-06-18 07:12:23 +0000

    Added user table and structured the schema

--- schemas.py
+++ schemas.py
@@ -29,3 +29,14 @@ class Book(BookBase):
     class Config:
         
         from_attributes = True
+
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class UserOut(BaseModel):
+    username: str
+    class Config:
+        orm_mode = True
+
